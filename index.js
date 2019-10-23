e773f2c1c98bc6b74572f4c8f1dec1b86ad41375
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-10-23 16:33:55 +0100

    fixed issue with low resolution screens

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.querySelector('#submit').addEventListener('click', function (e) {
     e.preventDefault()
+
     var loan = document.getElementById('loan').value
     var salary = document.getElementById('salary').value
     var APR = document.getElementById('APR').value
+
 
     if ((loan >= 6400) && (loan <= 7199)) {
         var adminFee = 500
@@ -30,26 +32,42 @@ document.querySelector('#submit').addEventListener('click', function (e) {
     finalPayment = Number(finalPayment).toFixed(2)
     upfrontFee = Number(upfrontFee).toFixed(2)
 
+
+    //validation for inputs
+    var validation = false
+    //output 3 error messages if all inputs are incorrect
     if (((loan <= 0) || (loan > 8000)) && ((APR < 10) || (APR > 100)) && ((salary < 10000) || (salary > 150000))) {
         document.querySelector('#output').innerHTML = ' <H2>Error</H2><p class="output">Loan must be between £1 and £8,000</p>' +
             '<p class="output">Salary must be between £10,000 and £150,000.</p>' +
             '<p class="output"> Monthly Repayment must be between 10% and 100%.</p>'
+        //output 2 error messages if loan amount and APR are incorrect
     } else if (((loan <= 0) || (loan > 8000)) && ((APR < 10) || (APR > 100))) {
         document.querySelector('#output').innerHTML = '<H2>Error</H2><p class="output">Loan must be between £1 and £8,000.</p>' +
             '<p class="output"> Monthly Repayment must be between 10% and 100%.</p>'
+        //output 2 error messages if salary amount and APR are incorrect
     } else if (((salary < 10000 || salary > 150000)) && ((APR < 10 || APR > 100))) {
         document.querySelector('#output').innerHTML = '<H2>Error</H2><p class="output">Salary must be between £10,000 and £150,000.</p>' +
             '<p class="output">Monthly Repayment must be between 10% and 100%.</p>'
+        //output 2 error messages if loan amount and salary are incorrect
     } else if (((loan <= 0) || (loan > 8000)) && ((salary < 10000) || (salary > 150000))) {
         document.querySelector('#output').innerHTML = '<H2>Error</H2><p class="output">Loan must be between £1 and £8,000</p>' +
             '<p class="output">Salary must be between £10,000 and £150,000.</p>'
+        //output error message if loan amount is incorrect
     } else if ((loan <= 0) || (loan > 8000)) {
         document.querySelector('#output').innerHTML = '<H2>Error</H2><p class="output">Loan must be between £1 and £8,000.</p>'
+        //output error message if salary is incorrect
     } else if ((salary < 10000) || (salary > 150000)) {
         document.querySelector('#output').innerHTML = '<H2>Error</H2><p class="output">Salary must be between £10,000 and £150,000.</p>'
+        //output error message if salary amount is incorrect
     } else if ((salary < 10000) || (salary > 150000)) {
         document.querySelector('#output').innerHTML = '<H2>Error</H2><p class="output">Salary must be between £10,000 and £150,000.</p>'
+        //set validation to true
     } else {
+        validation = true
+    }
+
+    //Loan repayment outputs
+    if (validation === true) {
 
         if (repaymentTime < 1) {
             repaymentTime = 1
@@ -57,22 +75,15 @@ document.querySelector('#submit').addEventListener('click', function (e) {
 
         if (monthlyPayment >= borrowedAmount) {
             document.querySelector('#output').innerHTML = '<h2> Borrowing Information</h2>' +
-                '<p class="output">UpFront Admin Fee: £' + upfrontFee + '.</p>' +
-                '<p class="output">Total Borrowed Amount: £' + borrowedAmount + '.</p>' +
+                '<p class="output">UpFront Admin Fee: £' + upfrontFee + '</p>' +
+                '<p class="output">Total Borrowed Amount: £' + borrowedAmount + '</p>' +
                 '<p class="output">Monthly payments of £' + borrowedAmount + '</p>'
         } else {
 
             document.querySelector('#output').innerHTML = '<h2> Borrowing Information</h2>' +
-                '<p class="output">UpFront Admin Fee: £' + upfrontFee + '.</p>' +
-                '<p class="output">Total Borrowed Amount: £' + borrowedAmount + '.</p>' +
-                '<p class="output">Monthly payments of £' + monthlyPayment + ' for ' + repaymentTime + ' months. One final payment of £' + finalPayment + '.' + '</p>'
+                '<p class="output">UpFront Admin Fee: £' + upfrontFee + '</p>' +
+                '<p class="output">Total Borrowed Amount: £' + borrowedAmount + '</p>' +
+                '<p class="output">Monthly payments of £' + monthlyPayment + ' for ' + repaymentTime + ' months. One final payment of £' + finalPayment + '</p>'
         }
     }
 })
-
-
-
-
-
-
-
